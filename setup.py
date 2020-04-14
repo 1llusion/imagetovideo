68187a340669610f3778a72971c790f118c22509
The Chris <chris@audioorigins.com>
2020-04-14 13:28:42 +0100

    Updated GitHub url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='Image To Video',
       version='0.1',
       description='Convert a set of images to a video',
-      url='',
+      url='https://github.com/1llusion/imagetovideo',
       author='Christopher Darlington',
       author_email='chris.darlington.music@gmail.com',
       license='MIT',
